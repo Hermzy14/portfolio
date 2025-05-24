292ceec2a9e87b56421a64cc45b8197b7eb1a910
Herman Lundby-Holen <hermanluho@gmail.com>
2025-05-24 11:20:30 +0200

    Added animation on page change.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,29 @@
 <script>
+	import { page } from '$app/state';
 	import Background from '../components/Background.svelte';
 	import Header from '../components/Header.svelte';
+	import { fade } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
+
+	let { children } = $props();
+
+	let previousPath = $state('');
+
+	$effect(() => {
+		if (page) {
+			previousPath = page.url.pathname;
+		}
+	});
 </script>
 
 <div id="wrapper">
 	<Header />
 	<main>
-		<slot />
+		{#key page.url.pathname}
+			<div in:fade={{ duration: 600, easing: cubicOut }}>
+				{@render children()}
+			</div>
+		{/key}
 	</main>
 </div>
 <Background />
@@ -17,10 +34,6 @@
 	:root {
 		--text-color: #f5efe5d9;
 		--heading-color: #f5efe5;
-		/* --background-color: #172015;
-		--primary-color: #cabda5;
-		--secondary-color: #eb662420;
-		--accent-color: #eb6624; */
 		--background-color: #131922;
 		--primary-color: #58a6ff20;
 		--secondary-color: #58a6ff20;
@@ -89,6 +102,8 @@
 
 	main {
 		width: 50%;
+		position: relative;
+		overflow: hidden;
 	}
 
 	@media (max-height: 1000px) {
