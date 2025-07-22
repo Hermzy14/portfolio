03e0edd050826bce2b09ea94100f13ea6ac42287
Herman Lundby-Holen <hermanluho@gmail.com>
2025-07-22 16:06:33 +0200

    Updated Chaos Game page.

--- src/routes/chaos-game/+page.svelte
+++ src/routes/chaos-game/+page.svelte
@@ -1,9 +1,8 @@
 <script>
+	import { SiGithub } from '@icons-pack/svelte-simple-icons';
 </script>
 
-<h1>Chaos Game</h1>
-<h2>Visualisering av fraktaler med JavaFX</h2>
-<a href="https://github.com/Hermzy14/Chaos-Game" target="_blank">GitHub</a>
+<h1>Chaos Game - Visualisering av fraktaler med JavaFX</h1>
 
 <p>
 	Dette prosjektet, utviklet i løpet av vårsemesteret 2024 ved NTNU i Ålesund, er en
@@ -12,7 +11,13 @@
 	brukergrensesnittet, implementering av fraktalgenereringslogikken, og håndtering av I/O.
 </p>
 
-<img src="/ChaosGameOverview.gif" alt="Oversikt over Chaos Game applikasjonen." />
+<a href="https://github.com/Hermzy14/Chaos-Game" target="_blank" class="github-link"
+	><SiGithub /> Sjekk ut på GitHub</a
+>
+
+<div class="img-container">
+	<img src="/ChaosGameOverview.gif" alt="Oversikt over Chaos Game applikasjonen." />
+</div>
 
 <h3>Oversikt av prosjektet</h3>
 <p>
@@ -23,21 +28,67 @@
 </p>
 
 <h3>Hovedfunksjoner og læringsutbytte</h3>
-<p>Prosjektet ga meg praktisk erfaring med:</p>
+<p id="experience">Prosjektet ga meg praktisk erfaring med:</p>
 <ul>
 	<li>
-		JavaFX GUI - Jeg lærte å bruke JavaFX for å lage et interaktivt brukergrensesnitt som lar
-		brukere kontrollere fraktalgenereringen.
+		JavaFX GUI: Jeg lærte å bruke JavaFX for å lage et interaktivt brukergrensesnitt som lar brukere
+		kontrollere fraktalgenereringen.
 	</li>
 	<li>
-		MVC - Jeg implementerte MVC-arkitekturen for å skille logikk, brukergrensesnitt og
+		MVC: Jeg implementerte MVC-arkitekturen for å skille logikk, brukergrensesnitt og
 		datahåndtering, noe som forbedret kodeorganiseringen og vedlikeholdbarheten.
 	</li>
 	<li>
 		Parametermanipulering: Applikasjonen lar brukere dynamisk justere ulike parametere. Denne
 		funksjonaliteten viser min evne til å lage fleksible og tilpassbare applikasjoner.
 	</li>
+	<li>
+		Data Persistence: Muligheten til å lagre og laste fraktaler fra filer viser min forståelse av
+		fil- og I/O-operasjoner.
+	</li>
 </ul>
 
 <style>
+	.img-container {
+		display: flex;
+		justify-content: center;
+	}
+	img {
+		border-radius: 8px;
+		margin: 1rem 0;
+	}
+
+	p {
+		text-align: justify;
+		margin: 0.5rem 0 1rem 0;
+	}
+	#experience {
+		margin-bottom: 0.5rem;
+	}
+
+	.github-link {
+		background: var(--text-color);
+		border-radius: 8px;
+		padding: 0.5rem 1rem;
+		gap: 0.5rem;
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--background-color);
+		text-decoration: none;
+		transition: all var(--theme-transition, 0.3s ease-in-out);
+	}
+	.github-link:hover {
+		background: var(--accent-color);
+		color: var(--text-color);
+	}
+
+	ul {
+		list-style: disc;
+		padding-left: 1rem;
+	}
+
+	li {
+		margin-bottom: 0.5rem;
+	}
 </style>
